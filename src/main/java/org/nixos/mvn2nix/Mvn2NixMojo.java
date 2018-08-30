fa44694d9ccc40ec18f08258b598504dde54050e
Thomas Tuegel <ttuegel@mailbox.org>
2018-08-30 09:36:58 -0500

    WIP: Add NixArtifact

--- src/main/java/org/nixos/mvn2nix/Mvn2NixMojo.java
+++ src/main/java/org/nixos/mvn2nix/Mvn2NixMojo.java
@@ -29,13 +29,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.execution.ProjectDependencyGraph;
 import org.apache.maven.plugin.AbstractMojo;
@@ -51,9 +51,17 @@ import org.apache.maven.model.Plugin;
 
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.repository.ArtifactRepository;
 import org.eclipse.aether.repository.LocalArtifactRequest;
 import org.eclipse.aether.repository.LocalRepository;
+import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.repository.WorkspaceRepository;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
+
+import org.nixos.mvn2nix.NixArtifact;
 
 
 /**
@@ -66,15 +74,15 @@ import org.eclipse.aether.repository.WorkspaceRepository;
        requiresDependencyResolution = ResolutionScope.TEST,
        requiresOnline = true,
        requiresProject = true
-		 )
+     )
 @Execute ( phase = LifecyclePhase.INSTALL )
 public class Mvn2NixMojo extends AbstractMojo
 {
     @Parameter(defaultValue="${session}", required=true)
     private MavenSession session;
 
-		@Parameter(defaultValue="${project}", required=true)
-		private MavenProject project;
+    @Parameter(defaultValue="${project}", required=true)
+    private MavenProject project;
 
     @Parameter(defaultValue="manifest.json", readonly=true)
     private File outputFile;
@@ -84,75 +92,134 @@ public class Mvn2NixMojo extends AbstractMojo
 
     private String getArtifactDownloadUrl(Artifact artifact)
     {
-        // We actually need to use repoSystem to re-resolve the remote
-        // repository for the artifact.
-        ArtifactRepository repo = artifact.getRepository();
         return "";
     }
 
-    static private Set<Artifact> artifacts = new HashSet<Artifact>();
+    static private Set<NixArtifact> artifacts =
+        new HashSet<NixArtifact>();
 
-		@Override
     public void execute()
         throws MojoExecutionException
     {
 
-				// Collect all artifacts from this project.
-				for (Artifact artifact: project.getArtifacts()) {
-						// If the artifact is remote, or cached from a remote, then
-						// resolve its download URL.
-						// See also:
-						//   org.eclipse.aether.RepositorySystem.resolveArtifact
-						//   org.apache.maven.project.MavenProject.getRemoteProjectRepositories
-						artifacts.add(artifact);
-				}
-				// Collect all plugin artifacts from this project.
-				for (Artifact artifact: project.getPluginArtifacts()) {
-						// If the artifact is remote, or cached from a remote, then
-						// resolve its download URL.
-						// See also:
-						//   org.eclipse.aether.RepositorySystem.resolveArtifact
-						//   org.apache.maven.project.MavenProject.getRemotePluginRepositories
-						artifacts.add(artifact);
-				}
+        // Collect all artifacts from this project.
+        for (Artifact artifact: project.getArtifacts()) {
+            // If the artifact is remote, or cached from a remote, then
+            // resolve its download URL.
+            // See also:
+            //   org.eclipse.aether.RepositorySystem.resolveArtifact
+            //   org.apache.maven.project.MavenProject.getRemoteProjectRepositories
+            resolve(artifact, project.getRemoteProjectRepositories()).ifPresent(
+                (NixArtifact a) -> artifacts.add(a)
+            );
+        }
+        // Collect all plugin artifacts from this project.
+        for (Artifact artifact: project.getPluginArtifacts()) {
+            // If the artifact is remote, or cached from a remote, then
+            // resolve its download URL.
+            // See also:
+            //   org.eclipse.aether.RepositorySystem.resolveArtifact
+            //   org.apache.maven.project.MavenProject.getRemotePluginRepositories
+            resolve(artifact, project.getRemotePluginRepositories()).ifPresent(
+                (NixArtifact a) -> artifacts.add(a)
+            );
+        }
 
         List<MavenProject> projects =
             session
             .getProjectDependencyGraph()
             .getSortedProjects();
 
-				if (project == projects.get(projects.size() - 1)) {
-						// This is the last project, so we should write the manifest.
-						try {
-								FileOutputStream output = new FileOutputStream(outputFile);
-								JsonGenerator generator = Json.createGenerator(output);
-								generator.writeStartArray();
-								for (Artifact artifact: artifacts) {
-										getLog()
-												.info("artifact "
-															+ artifact.getGroupId()
-															+ ":"
-															+ artifact.getArtifactId()
-															+ ":"
-															+ artifact.getVersion()
-														);
-										String url = getArtifactDownloadUrl(artifact);
-										generator
-												.writeStartObject()
-												.write("groupId", artifact.getGroupId())
-												.write("artifactId", artifact.getArtifactId())
-												.write("version", artifact.getVersion())
-												.write("url", url)
-												.writeEnd();
-								}
-								generator.writeEnd();
-								generator.close();
-								output.close();
-						} catch (FileNotFoundException e) {
-								throw new MojoExecutionException("Writing " + outputFile, e);
-						} catch (IOException e) {
-								throw new MojoExecutionException("Writing " + outputFile, e);
-						}
-				}
+        if (project == projects.get(projects.size() - 1)) {
+            // This is the last project, so we should write the manifest.
+            try {
+                FileOutputStream output = new FileOutputStream(outputFile);
+                JsonGenerator generator = Json.createGenerator(output);
+                generator.writeStartArray();
+                for (NixArtifact artifact: artifacts) {
+                    getLog().info("artifact " + artifact.getCoordinates());
+                    generator
+                        .writeStartObject()
+                        .write("groupId", artifact.groupId)
+                        .write("artifactId", artifact.artifactId)
+                        .write("version", artifact.version)
+                        .write("repo", artifact.repositoryUrl)
+                        .writeEnd();
+                }
+                generator.writeEnd();
+                generator.close();
+                output.close();
+            } catch (FileNotFoundException e) {
+                throw new MojoExecutionException("Writing " + outputFile, e);
+            } catch (IOException e) {
+                throw new MojoExecutionException("Writing " + outputFile, e);
+            }
+        }
+    }
+
+    /**
+     * Fully resolve the remote repository URL of an Artifact.
+     */
+    private Optional<NixArtifact>
+    resolve( Artifact artifact,
+             List<RemoteRepository> repos
+           ) {
+        ArtifactRequest request =
+            new ArtifactRequest(
+                toDefaultArtifact(artifact),
+                repos,
+                artifact.getScope()
+            );
+
+        ArtifactResult result = new ArtifactResult(request);
+        try {
+            result = repoSystem.resolveArtifact(
+                session.getRepositorySession(),
+                request
+            );
+        } catch (ArtifactResolutionException e) {
+            getLog().warn(
+                "Could not resolve artifact: "
+                + artifact.getGroupId() + ":"
+                + artifact.getArtifactId() + ":"
+                + artifact.getVersion() + " " + e
+            );
+        }
+
+        ArtifactRepository repo = result.getRepository();
+        if (repo instanceof RemoteRepository) {
+            RemoteRepository remote = (RemoteRepository) repo;
+            return Optional.of(
+                new NixArtifact( artifact.getGroupId(),
+                                 artifact.getArtifactId(),
+                                 artifact.getVersion(),
+                                 remote.getUrl()
+                               )
+            );
+        } else if (repo instanceof LocalRepository) {
+            // Artifact is local.
+            return Optional.empty();
+        } else if (repo instanceof WorkspaceRepository) {
+            // Artifact is local to this workspace.
+            return Optional.empty();
+        } else {
+            getLog().warn(
+                "Unknown repository type for artifact: "
+                + artifact.getGroupId() + ":"
+                + artifact.getArtifactId() + ":"
+                + artifact.getVersion()
+            );
+            return Optional.empty();
+        }
+    }
+
+    private DefaultArtifact toDefaultArtifact(Artifact artifact) {
+        return new DefaultArtifact(
+            artifact.getGroupId(),
+            artifact.getArtifactId(),
+            artifact.getClassifier(),
+            artifact.getArtifactHandler().getExtension(),
+            artifact.getVersion()
+        );
     }
 }
